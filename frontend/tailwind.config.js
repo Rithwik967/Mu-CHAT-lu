4127d611617cacb4e4a7425245d642f970e3e4fd
Rithwik967 <rithwik.stambamkadi@gmail.com>
2025-09-16 22:42:23 +0530

    new update after daisy v4

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./src/**/*.jsx",
-    "./src/**/*.js",
   ],
   theme: {
     extend: {
@@ -23,5 +21,47 @@ export default {
         },
       },
     },
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: [
+      "light",
+      "dark", 
+      "cupcake",
+      "bumblebee",
+      "emerald",
+      "corporate",
+      "synthwave",
+      "retro",
+      "cyberpunk",
+      "valentine",
+      "halloween",
+      "garden",
+      "forest",
+      "aqua",
+      "lofi",
+      "pastel",
+      "fantasy",
+      "wireframe",
+      "black",
+      "luxury",
+      "dracula",
+      "cmyk",
+      "autumn",
+      "business",
+      "acid",
+      "lemonade",
+      "night",
+      "coffee",
+      "winter",
+      "dim",
+      "nord",
+      "sunset",
+      "caramellatte",
+      "abyss",
+      "silk"
+    ],
   },
 }
